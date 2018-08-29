e12c412adcea2c76362188b1f08a8d3b89adf943
samyakducs <samyak.mcs18.du@gmail.com>
2018-08-29 22:11:10 +0530

    corrected the issue with inverted y axis

--- s1/mcs103/assignments/random_bee_walk/app.js
+++ s1/mcs103/assignments/random_bee_walk/app.js
@@ -1,6 +1,6 @@
 class Walker{
     constructor(x,y){
-        this.radius = 10
+        this.radius = 20
         this.x = x
         this.y = y
         this.prevx = x
@@ -8,23 +8,29 @@ class Walker{
     }
 
     display(){
+        //line from previous hexagon to current hexagon
+        strokeWeight(2)
         stroke(255,235,59,200)
         line(this.prevx, this.prevy, this.x, this.y)
+
+        //draw current hexagon
         stroke(255,255,255,150)
         fill(255,255,255,100)
         if((this.x == w / 2 && this.y == h / 2) || iterator > max_iteration){
-            stroke(255,235,59)
-            fill(255,235,59)
+            stroke(255,235,59,200)
+            fill(255,235,59,100)
         }
-        // strokeWeight(this.radius)
         polygon(this.x, this.y, this.radius, 6)
+        strokeWeight(this.radius / 2)
+        point(this.x, this.y)
 
+        //function to create the polygon
         function polygon(x, y, radius, npoints) {
             var angle = TWO_PI / npoints;
             beginShape();
             for (var a = 0; a < TWO_PI; a += angle) {
                 var sx = x + cos(a) * radius;
-                var sy = y + sin(a) * radius;
+                var sy = y - sin(a) * radius;
                 vertex(sx, sy);
             }
             endShape(CLOSE);
@@ -36,8 +42,9 @@ class Walker{
         let theta = (random_var * 60) + 30
         this.prevx = this.x
         this.prevy = this.y
-        this.x = this.x + 2 * this.radius * Math.cos(radians(theta - 60))
-        this.y = this.y + 2 * this.radius * Math.sin(radians(theta - 60))
+        this.x = this.x + 2 * this.radius * Math.cos(radians(theta))
+        //y is negated because the y axis is inverted
+        this.y = this.y - 2 * this.radius * Math.sin(radians(theta))
         console.log(theta)
     }
 }
@@ -45,22 +52,23 @@ class Walker{
 let bee
 let w = 600
 let h = 600
-max_iteration = 64
+max_iteration = 16
 iterator = 1
 
 function setup(){
     createCanvas(600,600)
     background(51)
-    frameRate(10)
+    // frameRate(1)
     bee = new Walker(width/2, height/2)
+    bee.display()
 }
 
 function draw(){
     // background(51)
-    bee.display()
-    bee.move()
+    iterator++
     if(iterator > max_iteration){
         noLoop()
     }
-    iterator++
+    bee.move()
+    bee.display()
 }
