0643528ffec1c7d536ff76f3509f0c393a148e65
samyakducs <samyak.mcs18.du@gmail.com>
2018-08-31 00:06:40 +0530

    added multiple iterations and added expected values

--- s1/mcs103/assignments/random_bee_walk/app.js
+++ s1/mcs103/assignments/random_bee_walk/app.js
@@ -1,6 +1,6 @@
 class Walker{
     constructor(x,y){
-        this.radius = 20
+        this.radius = 10
         this.x = x
         this.y = y
         this.prevx = x
@@ -16,7 +16,7 @@ class Walker{
         //draw current hexagon
         stroke(255,255,255,150)
         fill(255,255,255,100)
-        if((this.x == w / 2 && this.y == h / 2) || iterator > max_iteration){
+        if((this.x == w / 2 && this.y == h / 2) || walk_iterator >= walk_length){
             stroke(255,235,59,200)
             fill(255,235,59,100)
         }
@@ -27,13 +27,13 @@ class Walker{
         //function to create the polygon
         function polygon(x, y, radius, npoints) {
             var angle = TWO_PI / npoints;
-            beginShape();
+            beginShape()
             for (var a = 0; a < TWO_PI; a += angle) {
-                var sx = x + cos(a) * radius;
-                var sy = y - sin(a) * radius;
-                vertex(sx, sy);
+                var sx = x + cos(a) * radius
+                var sy = y - sin(a) * radius
+                vertex(sx, sy)
             }
-            endShape(CLOSE);
+            endShape(CLOSE)
         }
     }
 
@@ -45,31 +45,62 @@ class Walker{
         this.x = this.x + 2 * this.radius * Math.cos(radians(theta))
         //y is negated because the y axis is inverted
         this.y = this.y - 2 * this.radius * Math.sin(radians(theta))
-        console.log(theta)
     }
+
+    getX(){return this.x}
+    getY(){return this.y}
 }
 
 let bee
 let w = 600
 let h = 600
-max_iteration = 16
-iterator = 1
+let walk_length = 16
+let walk_iterator = 1
+let max_iterations = 500
+let iterator = 1
+let expected = 0
+let sixtyFourDone = false
 
 function setup(){
     let beehive = createCanvas(600,600)
     beehive.parent('beehive')
+
+    res1 = document.querySelector('#res1')
+    res3 = document.querySelector('#res3')
+    console.log(res1);
+
     background(51)
-    // frameRate(1)
+    frameRate(100)
     bee = new Walker(width/2, height/2)
-    bee.display()
 }
 
 function draw(){
-    // background(51)
-    iterator++
-    if(iterator > max_iteration){
-        noLoop()
-    }
-    bee.move()
+    background(51)
     bee.display()
+    if(iterator >= max_iterations){
+        noLoop()
+        if(!sixtyFourDone){
+            walk_length = 64
+            iterator = 1
+            sixtyFourDone = true
+            loop()
+        }
+    }
+    while(walk_iterator <= walk_length){
+        bee.move()
+        bee.display()
+        walk_iterator++
+    }
+
+    expected += dist(bee.getX(), bee.getY(), width / 2, height / 2) / max_iterations / bee.radius
+    console.log("dist = ", dist(bee.getX(), bee.getY(), width / 2, height / 2) / bee.radius,"expected = ", expected)
+    if(!sixtyFourDone){
+        res1.innerHTML = int(expected)
+    }else{
+        res3.innerHTML = int(expected)
+    }
+
+    bee = new Walker(width/2,height/2)
+    walk_iterator = 1
+    iterator++
 }
